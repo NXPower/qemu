ee2f88b96181e3c700e610ab9066c47cba915cfa
Mike Cui <cui@nutanix.com>
2015-11-21 06:19:58 -0800

    Turn off virtio-blk request merging.

--- hw/block/virtio-blk.c
+++ hw/block/virtio-blk.c
@@ -1021,7 +1021,7 @@ static Property virtio_blk_properties[] = {
     DEFINE_PROP_BIT("scsi", VirtIOBlock, conf.scsi, 0, false),
 #endif
     DEFINE_PROP_BIT("request-merging", VirtIOBlock, conf.request_merging, 0,
-                    true),
+                    false),
     DEFINE_PROP_UINT16("num-queues", VirtIOBlock, conf.num_queues, 1),
     DEFINE_PROP_UINT16("queue-size", VirtIOBlock, conf.queue_size, 128),
     DEFINE_PROP_LINK("iothread", VirtIOBlock, conf.iothread, TYPE_IOTHREAD,
